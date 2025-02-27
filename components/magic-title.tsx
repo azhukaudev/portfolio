26b40d9807586fba3c2cc61f4e4facba2526975b
azhukaudev <azhukaudev@gmail.com>
2025-02-27 18:52:31 +0100

    feat: improve magic-title component

--- components/magic-title.tsx
+++ components/magic-title.tsx
@@ -1,11 +1,15 @@
-export default function MagicTitle({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import { memo } from 'react';
+
+export interface MagicTitleProps {
+  children: string;
+}
+
+const MagicTitle = memo(function MagicTitle({ children }: MagicTitleProps) {
   return (
     <span className="bg-gradient-to-r from-blue-500 to-emerald-300 bg-clip-text text-6xl leading-tight font-extrabold text-transparent">
       {children}
     </span>
   );
-}
+});
+
+export default MagicTitle;
